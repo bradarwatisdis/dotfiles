76d7c16f9d18059cafb194a88096c6d01fd6a74b
bradarwatisdis <sd.paula@outlook.com>
2026-07-22 16:34:00 -0300

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+vim.o.number = true
+vim.o.syntax = false
+vim.o.tabstop = 2 
+vim.o.shiftwidth = 2 
+
+vim.keymap.set('n', '<space>n', ':w!<cr>')
+vim.keymap.set('n', '<space>m', ':q!<cr>')
+vim.keymap.set('n', '<space>g', ':term<cr>')
